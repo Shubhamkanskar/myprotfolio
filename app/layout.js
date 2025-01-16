b044193e804f9f615da14d1b124eb78ddbe2ec04
shubham_kanaskar <shubhamkanaskar75@gmail.com>
2025-01-16 14:37:54 +0530

    added some ui fixes😶‍🌫️

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,9 @@
-import localFont from "next/font/local";
+
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Shubham Kanaskar - Full Stack Engineer",
@@ -21,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black text-white`}
+        className={`{inter.className}`}
       >
         {children}
       </body>
